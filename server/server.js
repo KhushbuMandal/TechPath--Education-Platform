9bb556e6c35ee8f054c93666032a4d6c45822a11
KhushbuMandal <khushbumandal4459@gmail.com>
2024-07-12 23:03:43 +0530

    Changed client url in server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 //lets tackle cors
 const corsOptions = {
-    origin: "http://localhost:5173",
+    //origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods : "GET , POST , PUT , DELETE , PATCH , HEAD ",
     credentials : true
 };
